02a202f92908756aa9e2329c450423bd6e7bb549
nevesrodrigo2 <nevesrodrigo2@gmail.com>
2025-07-07 00:19:55 +0100

    added reusable messages for common notifications and logging

---
+++ messages/error_messages.py
@@ -0,0 +1,17 @@
+# User
+USER_NOT_FOUND = "User not found"
+OLD_PASSWORD_INCORRECT = "Old password not correct"
+NEW_PASSWORD_MISMATCH = "New password does not match the confirmation password"
+NEW_PASSWORD_SAME_AS_OLD = "New password is the same as the old password"
+USER_DOES_NOT_EXIST = "User does not exist"
+
+# Ratings
+RATING_NOT_FOUND = "Rating not found"
+RATINGS_NOT_FOUND = "No ratings found for this user"
+RATING_CREATION_FAILED = "Failed to create rating"
+RATING_ALREADY_EXISTS = "Rating already exists for this album by the user"
+
+# Album
+ALBUM_CREATION_FAILED = "Failed to create album"
+ALBUM_ALREADY_EXISTS = "Album already exists"
+ALBUM_NOT_FOUND = "Album not found"
---
+++ messages/success_messages.py
@@ -0,0 +1,9 @@
+# RATINGS
+RATING_CREATION_SUCCESS = "Rating created successfully"
+RATING_DELETION_SUCCESS = "Rating deleted successfully"
+ALL_RATINGS_DELETION_SUCCESS = "All ratings deleted successfully"
+RATING_UPDATE_SUCCESS = "Rating updated successfully"
+
+
+# ALBUMS
+ALBUM_DATABASE_INSERTION_SUCCESS = "Album inserted into the database successfully"
